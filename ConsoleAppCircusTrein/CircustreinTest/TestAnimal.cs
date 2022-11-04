8bfa7a9ddb2ca10d0b25cfd9c783335308be2999
MatthijsDolmans <95029345+MatthijsDolmans@users.noreply.github.com>
2022-11-04 10:16:14 +0100

    Add files via upload

---
+++ ConsoleAppCircusTrein/CircustreinTest/TestAnimal.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleAppCircusTrein;
+
+namespace CircustreinTest
+
+{
+    [TestClass]
+    public class TestAnimal
+    {
+        [TestMethod]
+        public void CreateAnimalSize()
+        {
+            Animal animal = new Animal(Animal.AnimalType.Herbivore, Animal.AnimalSize.Medium);
+
+            Assert.AreEqual(3, (int)animal.GetSize());
+        }
+        [TestMethod]
+        public void TestAnimalEatingEachother()
+        {
+            Animal animal1 = new Animal(Animal.AnimalType.Carnivore, Animal.AnimalSize.Medium);
+            Wagon wagon = new Wagon();
+            wagon.AnimalsInWagon.Add(animal1);
+
+            Assert.AreEqual(false, animal1.CheckIfAnimalCanEat(wagon));
+        }
+    }
+}
---
+++ ConsoleAppCircusTrein/ConsoleAppCircusTrein/Wagon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleAppCircusTrein.Animal;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace ConsoleAppCircusTrein
+{
+    public class Wagon
+    {
+        public List<Animal> AnimalsInWagon { get; private set; }
+        public int wagonNumber { get; private set; }
+
+        private static int wagonCounter = 1;
+
+        public Wagon()
+        {
+            wagonNumber = wagonCounter;
+            wagonCounter++;
+            AnimalsInWagon = new List<Animal>();
+        }
+           
+       
+        public bool CheckIfAnimalFits(Animal a)
+        {
+            int usedcapacity = 0;
+            foreach (var animal in AnimalsInWagon)
+            {
+                usedcapacity = usedcapacity + (int)animal.GetSize();
+            }
+            if ((int)a.GetSize() + usedcapacity > 10)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+      
+    }
+}
